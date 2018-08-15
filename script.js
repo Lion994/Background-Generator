580d6f218b456600faebaf8c8d23c1b69261f26d
Lion994 <31659950+Lion994@users.noreply.github.com>
2018-08-15 12:21:00 +0200

    installatiopn de lodash et browserfy donc script.js devient bundle.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const _ = require('lodash'); // with lodash the syntax isn't import but require.....
+let array = [1,2,3,4,5,6,7,8];
+console.log('answer: ',_.without(array, 3));
+
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
 
 function setGradient(){
   body.style.background = "linear-gradient(to right, "+ color1.value+ ", "+ color2.value+ ")";
